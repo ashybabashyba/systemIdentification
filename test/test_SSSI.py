ef0bf7b65f8370dd9297c94999b10d5d42145556
ashybabashyba <cramoss@unal.edu.co>
2026-02-12 18:20:22 +0100

    Add test for state space construction and validate dimensions

--- test/test_SSSI.py
+++ test/test_SSSI.py
@@ -50,3 +50,22 @@ def test_output_Hankel_matrix_construction():
 
     for i in range(outputHankelMatrix.shape[1] - 1):
         assert np.allclose(outputHankelMatrix[:-1, i+1], outputHankelMatrix[1:, i])
+
+def test_state_space_construction_and_dimensions():
+    inputTimeVector = np.arange(0, 10, 0.1)
+    outputTimeVector = np.arange(0, 10, 0.2)
+    inputSignal = np.sin(inputTimeVector)
+    outputSignal = np.cos(outputTimeVector)
+
+    system = SystemIdentificationWrapper(timeInput=inputTimeVector, timeOutput=outputTimeVector)
+    system.addInputData(inputSignal)
+    system.buildInterpolatedInputValues()
+    system.addOutputData(outputSignal)
+
+    stateSpace = StateSpace(systemInput=system.interpolatedInputValues[0], systemOutput=system.outputValues)
+    A, B, C, D, initialState = stateSpace.buildStateSpaceSystem()
+
+    assert A.shape[0] == A.shape[1]
+    assert B.shape[0] == A.shape[0] and B.shape[1] == 1
+    assert C.shape[1] == A.shape[0] and C.shape[0] == 1
+    assert D.shape[0] == 1 and D.shape[1] == 1
