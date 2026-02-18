2341f7372a59c84ebea01d2456983b2acef9a142
ashybabashyba <carlosjramos812@gmail.com>
2026-02-18 20:09:43 +0100

    Added plots to save data and output figures

--- testData/rogowskiCoil/prepost.py
+++ testData/rogowskiCoil/prepost.py
@@ -51,23 +51,27 @@ A, B, C, D, initialState = stateSpace.buildStateSpaceSystem()
 
 ## Plotting initial input and reconstructed training output ##
 
-plt.plot(system.timeOutput, system.interpolatedInputValues[0], label='Input Current')
-plt.xlabel('Time')
-plt.ylabel('Current')
-# plt.xlim((0, 2e-10))
+plt.plot(system.timeOutput*1e9, system.interpolatedInputValues[0], label='Input: Current on Nodal Source')
+plt.xlabel('Time (ns)')
+plt.ylabel('Current (A)')
+# plt.xlim((0, 2e-9))
 plt.legend()
 plt.grid()
+plt.savefig("Rogowski_input_data.png", dpi=300, bbox_inches='tight')
 plt.show()
 
 
 xid, yid = stateSpace.evolveInput(A=A, B=B, C=C, D=D, u=system.interpolatedInputValues[0], x0=initialState)
-plt.plot(system.timeOutput, system.outputValues[0], label='Original Output')
-plt.plot(system.timeOutput, yid[0], '--', label='Output Reconstructed with SSSI')
-plt.xlabel('Time')
-plt.ylabel('Current')
+
+plt.plot(system.timeOutput*1e9, system.outputValues[0]*1e3, color='r', label='Output: Current on Coil')
+# plt.plot(system.timeOutput*1e9, yid[0]*1e3, '--', label='Output Reconstructed with SSSI')
+plt.xlabel('Time (ns)')
+plt.ylabel('Current (mA)')
 plt.legend()
 plt.grid()
+plt.savefig("Rogowski_output_data.png", dpi=300, bbox_inches='tight')
 plt.show()
+
 
 # %% Prediction with the previous parameters
 
@@ -265,4 +269,186 @@ plt.ylabel('Relative Spectral Error')
 plt.title('Spectral Error vs Energy Percentage')
 plt.grid()
 plt.show()
+# %% Generating comparison between methods
+
+## Loading training/prediction data and defining time vectors ##
+
+step = 0.01e-9
+initialTrainingTime = 0
+finalTrainingTime = 20e-9
+newTimeVector = np.arange(initialTrainingTime, finalTrainingTime + step, step)
+
+initialTrainingTime_Naishadham = 0e-9
+finalTrainingTime_Naishadham = 20e-9
+newTimeVector_Naishadham = np.arange(initialTrainingTime_Naishadham, finalTrainingTime_Naishadham + step, step)
+
+
+data = np.load("large_gaussian_data.npz")
+
+time_input = data["time_input"]
+input_signal = data["input_signal"]
+time_output_raw = data["time_output"]
+output_signal_raw = data["output_signal"]
+
+## Defining system for Naishadham method ##
+
+system_Naishadham = SystemIdentificationWrapper(
+    timeInput=time_input,
+    timeOutput=newTimeVector_Naishadham
+)
+
+system_Naishadham.addInputData(input_signal)
+system_Naishadham.buildInterpolatedInputValues()
+system_Naishadham.addOutputData(
+    np.interp(newTimeVector_Naishadham, time_output_raw, output_signal_raw)
+)
+
+stateSpace_Naishadham = StateSpace(systemInput = system_Naishadham.interpolatedInputValues[0],
+                                   systemOutput = system_Naishadham.outputValues,
+                                   energyThreshold=1-1e-9,
+                                   observabilityMethod='Naishadham')
+
+A_Naishadham, B_Naishadham, C_Naishadham, D_Naishadham, initialState_Naishadham = stateSpace_Naishadham.buildStateSpaceSystem()
+
+
+## Defining system for Juang method ##
+
+system_Juang = SystemIdentificationWrapper(
+    timeInput=time_input,
+    timeOutput=newTimeVector
+)
+
+system_Juang.addInputData(input_signal)
+system_Juang.buildInterpolatedInputValues()
+system_Juang.addOutputData(
+    np.interp(newTimeVector, time_output_raw, output_signal_raw)
+)
+
+stateSpace_Juang = StateSpace(systemInput = system_Juang.interpolatedInputValues[0],
+                              systemOutput = system_Juang.outputValues,
+                              energyThreshold = 1-1e-9,
+                              observabilityMethod = 'Juang')
+
+A_Juang, B_Juang, C_Juang, D_Juang, initialState_Juang = stateSpace_Juang.buildStateSpaceSystem()
+
+
+## Defining system for Projection method ##
+
+system_Projection = SystemIdentificationWrapper(
+    timeInput=time_input,
+    timeOutput=newTimeVector)
+
+system_Projection.addInputData(input_signal)
+system_Projection.buildInterpolatedInputValues()
+system_Projection.addOutputData(
+    np.interp(newTimeVector, time_output_raw, output_signal_raw)
+)
+
+stateSpace_Projection = StateSpace(systemInput = system_Projection.interpolatedInputValues[0],
+                              systemOutput = system_Projection.outputValues,
+                              energyThreshold=1-1e-9,
+                              observabilityMethod='Projection')
+
+A_Projection, B_Projection, C_Projection, D_Projection, initialState_Projection = stateSpace_Projection.buildStateSpaceSystem()
+
+
+## Evolving systems  ##
+
+finalTime = np.arange(0, 45e-9 + step, step)
+finalOutput = np.interp(
+    finalTime,
+    data["time_output"],
+    data["output_signal"]
+).reshape((1, -1))
+
+finalInput = np.interp(
+    finalTime,
+    data["time_input"],
+    data["input_signal"]
+).reshape((1, -1))
+
+_, y_id_predicted_Naishadham = stateSpace_Naishadham.evolveInput(A=A_Naishadham, B=B_Naishadham, C=C_Naishadham, D=D_Naishadham, u=finalInput[0], x0=initialState_Naishadham)
+_, y_id_predicted_Juang = stateSpace_Juang.evolveInput(A=A_Juang, B=B_Juang, C=C_Juang, D=D_Juang, u=finalInput[0], x0=initialState_Juang)
+_, y_id_predicted_Projection = stateSpace_Projection.evolveInput(A=A_Projection, B=B_Projection, C=C_Projection, D=D_Projection, u=finalInput[0], x0=initialState_Projection)
+
+
+# %%
+## Plotting results ##
+
+colors = ['k', 'C0', 'C2', 'C3']
+markers = [None, 'o', 's', '^']
+labels = ['Original Output', 
+          'Naishadham method Output', 
+          'Juang method Output', 
+          'Projection method Output']
+
+fig, axs = plt.subplots(3, 1, figsize=(6, 8))
+
+time_ns = finalTime*1e9 
+original = finalOutput[0]*1e3 
+naishadham = y_id_predicted_Naishadham[0]*1e3  
+juang = y_id_predicted_Juang[0]*1e3  
+projection = y_id_predicted_Projection[0]*1e3  
+
+# -------------------------
+# 1️⃣ Original vs Naishadham
+# -------------------------
+axs[0].plot(time_ns, original, color=colors[0], linewidth=2, label=labels[0])
+axs[0].plot(time_ns, naishadham, color=colors[1], label=labels[1])
+axs[0].grid()
+axs[0].legend()
+
+# -------------------------
+# 2️⃣ Original vs Juang
+# -------------------------
+axs[1].plot(time_ns, original, color=colors[0], linewidth=2, label=labels[0])
+axs[1].plot(time_ns, juang, color=colors[2], label=labels[2])
+axs[1].grid()
+axs[1].legend()
+
+# -------------------------
+# 3️⃣ Original vs Projection
+# -------------------------
+axs[2].plot(time_ns, original, color=colors[0], linewidth=2, label=labels[0])
+axs[2].plot(time_ns, projection, color=colors[3], label=labels[3])
+axs[2].grid()
+axs[2].legend()
+
+# Etiquetas globales
+for ax in axs.flat:
+    ax.set_xlabel("Time (ns)")
+    ax.set_ylabel("Current (mA)")
+
+plt.tight_layout()
+plt.savefig("Rogowski_output_comparison.png", dpi=300, bbox_inches='tight')
+plt.show()
+
+
+# -------------------------
+# 4️⃣ Zoom comparison
+# -------------------------
+plt.plot(time_ns, original, color=colors[0], linewidth=2, label=labels[0])
+plt.plot(time_ns, juang, '--s', color=colors[2],
+              markersize=4, markevery=15, markerfacecolor='none',
+              label=labels[2])
+plt.plot(time_ns, projection, '--^', color=colors[3],
+              markersize=4, markevery=15, markerfacecolor='none',
+              label=labels[3])
+
+
+plt.xlim(30, 40)
+mask = (finalTime >= 30e-9) & (finalTime <= 40e-9)
+y_zoom = original[mask]
+plt.ylim(min(y_zoom), max(y_zoom))
+
+plt.grid()
+plt.legend()
+plt.xlabel('Time (ns)')
+plt.ylabel('Current (mA)')
+
+
+plt.tight_layout()
+plt.savefig("Rogowski_output_comparison_zoom.png", dpi=300, bbox_inches='tight')
+plt.show()
+
 # %%
