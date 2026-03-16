ecd8f1bb190dcde6f34549c6267fe5f6f875ca2a
ashybabashyba <cramoss@unal.edu.co>
2026-03-16 09:36:41 +0100

    Added RLC multioutput system test

--- test/test_SSSI.py
+++ test/test_SSSI.py
@@ -251,3 +251,67 @@ def test_Rogowski_eigenvalues():
     A, B, C, D, initialState = stateSpace.buildStateSpaceSystem()
 
     assert np.all(np.abs(np.linalg.eigvals(A)) < 1)
+
+def test_RLC_multioutput_input_output_comparison():
+    step = 0.01e-3
+    initialTrainingTime = 0
+    finalTrainingTime = 1.0e-3
+    newTimeVector = np.arange(initialTrainingTime, finalTrainingTime + step, step)
+
+    trainingFile = "testData/RLC_circuit/RLC_circuit_modulated_gaussian.txt"
+    predictionFile = "testData/RLC_circuit/RLC_circuit_modulated_gaussian_final.txt"
+
+    resistor_voltage_file = "testData/RLC_circuit/RLC_voltage_at_R"
+    inductor_voltage_file = "testData/RLC_circuit/RLC_voltage_at_L"
+    capacitor_voltage_file = "testData/RLC_circuit/RLC_voltage_at_C"
+
+    system = SystemIdentificationWrapper(timeInput=np.loadtxt(trainingFile, usecols=0, skiprows=1),
+                                        timeOutput=newTimeVector)
+
+    system.addInputData(np.loadtxt(trainingFile, usecols=1, skiprows=1))
+    system.buildInterpolatedInputValues()
+
+    ## Resistor data ##
+    system.addOutputData(np.interp(newTimeVector, 
+                                np.loadtxt(trainingFile, skiprows=1, usecols=0), 
+                                np.loadtxt(trainingFile, skiprows=1, usecols=2)))
+    system.addOutputData(np.interp(newTimeVector, 
+                                np.loadtxt(resistor_voltage_file, skiprows=1, usecols=0), 
+                                np.loadtxt(resistor_voltage_file, skiprows=1, usecols=1))) 
+
+    ## Inductor data ##
+    system.addOutputData(np.interp(newTimeVector, 
+                                np.loadtxt(trainingFile, skiprows=1, usecols=0), 
+                                np.loadtxt(trainingFile, skiprows=1, usecols=2)))
+    system.addOutputData(np.interp(newTimeVector, 
+                                np.loadtxt(inductor_voltage_file, skiprows=1, usecols=0), 
+                                np.loadtxt(inductor_voltage_file, skiprows=1, usecols=1)))
+
+    ## Capacitor data ##
+    system.addOutputData(np.interp(newTimeVector, 
+                                np.loadtxt(trainingFile, skiprows=1, usecols=0), 
+                                np.loadtxt(trainingFile, skiprows=1, usecols=2)))
+    system.addOutputData(np.interp(newTimeVector, 
+                                np.loadtxt(capacitor_voltage_file, skiprows=1, usecols=0), 
+                                np.loadtxt(capacitor_voltage_file, skiprows=1, usecols=1)))
+
+    stateSpace = StateSpace(systemInput = system.interpolatedInputValues[0],
+                            systemOutput = system.outputValues)
+
+    A, B, C, D, initialState = stateSpace.buildStateSpaceSystem()
+
+    finalTime = np.arange(0, 5e-3 + step, step)
+    finalOutput = np.interp(finalTime, 
+                    np.loadtxt(predictionFile, skiprows=1, usecols=0), 
+                    np.loadtxt(predictionFile, skiprows=1, usecols=1)).reshape((1, -1))
+
+    finalInput = np.interp(finalTime, 
+                    np.loadtxt(trainingFile, usecols=0, skiprows=1), 
+                    np.loadtxt(trainingFile, usecols=1, skiprows=1)).reshape((1, -1))
+
+    x_id_predicted, y_id_predicted = stateSpace.evolveInput(A=A, B=B, C=C, D=D, u=finalInput[0], x0=initialState)
+
+    assert np.allclose(-finalInput[0], y_id_predicted[1] + y_id_predicted[3] + y_id_predicted[5], atol=1e-3)
+    assert np.allclose(finalOutput[0], y_id_predicted[0], atol=1e-4)
+    assert np.allclose(finalOutput[0], y_id_predicted[2], atol=1e-4)
+    assert np.allclose(finalOutput[0], y_id_predicted[4], atol=1e-4)
